bd5c8bc962985426de6bf85774fc9501c566c10f
peck liu <xiaoyang20170204@gmail.com>
2017-04-08 19:17:36 +0800

    step1

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,44 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(jobs_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(jobs_params)
+      redirect_to jobs_path
+    else
+      rendr :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+  private
+  def jobs_params
+    params.require(:job).permit(:title,:description)
+  end
+end
