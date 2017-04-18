32d77b97941f3fb85c7663e6456388d41ac77040
peck liu <xiaoyang20170204@gmail.com>
2017-04-18 22:28:15 +0800

    alert bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
-
-  def set_locale
-    # 可以将 ["en", "zh-TW"] 设定为 VALID_LANG 放到 config/environment.rb 中
-    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
-      session[:locale] = params[:locale]
-    end
-
-    I18n.locale = session[:locale] || I18n.default_locale
-  end
 
   def require_is_admin
     if !current_user.admin?
