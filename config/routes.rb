e45ed4e9a75621b4878e9e493ced3b8701f7ca33
peck liu <xiaoyang20170204@gmail.com>
2017-04-19 21:57:51 +0800

    sign up customed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcomes#index'
+  devise_for :users, controllers: {
+         registrations: 'users/registrations'
+       }
+       root 'welcomes#index'
 
   namespace :admin do
     resources :jobs do
