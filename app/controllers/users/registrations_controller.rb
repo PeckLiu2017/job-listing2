8d1a44cd6ed239218cbf3ce6f83228e1c5deb938
peck liu <xiaoyang20170204@gmail.com>
2017-04-21 03:42:05 +0800

    beautyfy weclome index and navbar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:is_admin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:is_admin,:username])
   end
 
   #If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar,:username])
   end
 
   # The path used after sign up.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419154558) do
+ActiveRecord::Schema.define(version: 20170420154537) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170419154558) do
     t.boolean  "is_candidate",           default: true
     t.string   "timestamp"
     t.string   "avatar"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
